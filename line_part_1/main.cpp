8e0914ec6f4d0892908ac91eda3e010041837f7c
pnghfng <pnghfng52388@icloud.com>
2025-09-05 15:13:57 +0800

    add line project

---
+++ line_part_1/main.cpp
@@ -0,0 +1,211 @@
+#include "tgaimage.hpp"
+#include <cmath>
+
+constexpr TGAColor white = {255, 255, 255, 255}; // attention, BGRA order
+constexpr TGAColor green = {0, 255, 0, 255};
+constexpr TGAColor red = {0, 0, 255, 255};
+constexpr TGAColor blue = {255, 128, 64, 255};
+constexpr TGAColor yellow = {0, 200, 255, 255};
+
+void line_0(int ax, int ay, int bx, int by, TGAImage& framebuffer, TGAColor color)
+{
+    for (float t = 0.; t < 1.; t += .02)
+    {
+        int x = std::round(ax + (bx - ax) * t);
+        int y = std::round(ay + (by - ay) * t);
+        framebuffer.set(x, y, color);
+    }
+}
+
+// still insufficient sampling
+void line_1(int ax, int ay, int bx, int by, TGAImage& framebuffer, TGAColor color)
+{
+    if (ax > bx)
+    { // make it left−to−right
+        std::swap(ax, bx);
+        std::swap(ay, by);
+    }
+    for (int x = ax; x <= bx; x++)
+    {
+        float t = (x - ax) / static_cast<float>(bx - ax);
+        int y = std::round(ay + (by - ay) * t);
+        framebuffer.set(x, y, color);
+    }
+}
+
+// DDA算法绘制直线 考虑微分增量
+// y = k * x + b
+// 判断k的大小
+// 如果k大于1，则说明y变化更快，应该基于y的递增进行采样
+void line_2(int ax, int ay, int bx, int by, TGAImage& framebuffer, TGAColor color)
+{
+    bool steep = std::abs(ax - bx) < std::abs(ay - by);
+    if (steep)
+    { // if the line is steep, we transpose the image
+        std::swap(ax, ay);
+        std::swap(bx, by);
+    }
+    if (ax > bx)
+    { // make it left−to−right
+        std::swap(ax, bx);
+        std::swap(ay, by);
+    }
+    for (int x = ax; x <= bx; x++)
+    {
+        float t = (x - ax) / static_cast<float>(bx - ax);
+        int y = std::round(ay + (by - ay) * t);
+        if (steep) // if transposed, de−transpose
+            framebuffer.set(y, x, color);
+        else
+            framebuffer.set(x, y, color);
+    }
+}
+
+void line_3(int ax, int ay, int bx, int by, TGAImage& framebuffer, TGAColor color)
+{
+    bool steep = std::abs(ax - bx) < std::abs(ay - by);
+    if (steep)
+    { // if the line is steep, we transpose the image
+        std::swap(ax, ay);
+        std::swap(bx, by);
+    }
+    if (ax > bx)
+    { // make it left−to−right
+        std::swap(ax, bx);
+        std::swap(ay, by);
+    }
+    int y = ay;
+    float error = 0;
+    for (int x = ax; x <= bx; x++)
+    {
+        if (steep) // if transposed, de−transpose
+            framebuffer.set(y, x, color);
+        else
+            framebuffer.set(x, y, color);
+        error += std::abs(by - ay) / static_cast<float>(bx - ax);
+        if (error > .5)
+        {
+            y += by > ay ? 1 : -1;
+            error -= 1.;
+        }
+    }
+}
+
+// 优化后避免浮点运算，中点Brensenham
+void line_4(int ax, int ay, int bx, int by, TGAImage& framebuffer, TGAColor color)
+{
+    bool steep = std::abs(ax - bx) < std::abs(ay - by);
+    if (steep)
+    { // if the line is steep, we transpose the image
+        std::swap(ax, ay);
+        std::swap(bx, by);
+    }
+    if (ax > bx)
+    { // make it left−to−right
+        std::swap(ax, bx);
+        std::swap(ay, by);
+    }
+    int y = ay;
+    int ierror = 0;
+    for (int x = ax; x <= bx; x++)
+    {
+        if (steep) // if transposed, de−transpose
+            framebuffer.set(y, x, color);
+        else
+            framebuffer.set(x, y, color);
+        ierror += 2 * std::abs(by - ay);
+        if (ierror > bx - ax)
+        {
+            y += by > ay ? 1 : -1;
+            ierror -= 2 * (bx - ax);
+        }
+    }
+}
+
+// 最终优化
+void line_5(int ax, int ay, int bx, int by, TGAImage& framebuffer, TGAColor color)
+{
+    bool steep = std::abs(ax - bx) < std::abs(ay - by);
+    if (steep)
+    { // if the line is steep, we transpose the image
+        std::swap(ax, ay);
+        std::swap(bx, by);
+    }
+    if (ax > bx)
+    { // make it left−to−right
+        std::swap(ax, bx);
+        std::swap(ay, by);
+    }
+    int y = ay;
+    int ierror = 0;
+    for (int x = ax; x <= bx; x++)
+    {
+        if (steep) // if transposed, de−transpose
+            framebuffer.set(y, x, color);
+        else
+            framebuffer.set(x, y, color);
+        ierror += 2 * std::abs(by - ay);
+        y += (by > ay ? 1 : -1) * (ierror > bx - ax);
+        ierror -= 2 * (bx - ax) * (ierror > bx - ax);
+    }
+}
+
+void line(int ax, int ay, int bx, int by, TGAImage& framebuffer, TGAColor color)
+{
+    bool steep = abs(bx - ax) < abs(by - ay);
+    if (steep) // 确保始终是对x进行增量
+    {
+        std::swap(by, bx);
+        std::swap(ay, ax);
+    }
+    if (ax > bx) // 确保是从左往右进行遍历
+    {
+        std::swap(ax, bx);
+        std::swap(ay, by);
+    }
+    int y = ay;
+    int d = 0; // f(x + 1, y + 0.5)
+    for (int x = ax; x <= bx; x++)
+    {
+
+        if (steep) // 绘制
+        {
+            framebuffer.set(y, x, color);
+        }
+        else
+        {
+            framebuffer.set(x, y, color);
+        }
+        if (d > 0)
+        {
+            y = y + 1;
+            d += bx - ax;
+        }
+        d += by - ay;
+    }
+}
+
+int main(int argc, char** argv)
+{
+    constexpr int width = 64;
+    constexpr int height = 64;
+    TGAImage framebuffer(width, height, TGAImage::RGB);
+
+    int ax = 7, ay = 3;
+    int bx = 12, by = 37;
+    int cx = 62, cy = 53;
+
+    line(ax, ay, bx, by, framebuffer, blue);
+    line(cx, cy, bx, by, framebuffer, green);
+    line(cx, cy, ax, ay, framebuffer, yellow);
+    line(ax, ay, cx, cy, framebuffer, red);
+
+    framebuffer.set(ax, ay, white);
+    framebuffer.set(bx, by, white);
+    framebuffer.set(cx, cy, white);
+
+    framebuffer.write_tga_file("framebuffer.tga");
+    system("open framebuffer.tga");
+
+    return 0;
+}
