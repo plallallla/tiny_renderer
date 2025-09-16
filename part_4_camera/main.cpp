f6ae9f0ed98f01dd8449daf3eec107a10f0b4901
pnghfng <pnghfng52388@icloud.com>
2025-09-16 15:01:52 +0800

    更新相机部分，增加渲染部分

--- part_4_camera/main.cpp
+++ part_4_camera/main.cpp
@@ -14,51 +14,55 @@ mat<4, 4> Model_View_M;
 mat<4, 4> Viewport_M;
 mat<4, 4> Perspective_M;
 
-double signed_triangle_area(int ax, int ay, int bx, int by, int cx, int cy)
-{
-    return .5 * ((by - ay) * (bx + ax) + (cy - by) * (cx + bx) + (ay - cy) * (ax + cx));
-}
-
 void rasterize(const vec4 clip[3], std::vector<double>& zbuffer, TGAImage& framebuffer, const TGAColor color)
 {
+    // 转换为归一化坐标 ncd = clip/w
     vec4 ndc[3] = 
     {
         clip[0] / clip[0].w, 
         clip[1] / clip[1].w, 
         clip[2] / clip[2].w
-    }; // normalized device coordinates
+    };
+    // 转换为屏幕坐标 screen = viewport*ndc
     vec2 screen[3] = 
     {
         (Viewport_M * ndc[0]).xy(), 
         (Viewport_M * ndc[1]).xy(),
         (Viewport_M * ndc[2]).xy()
-    }; // screen coordinates
-
+    };
+    // 面积矩阵 用于剔除背面三角形与面积过小三角形
     mat<3, 3> ABC = {{{screen[0].x, screen[0].y, 1.}, {screen[1].x, screen[1].y, 1.}, {screen[2].x, screen[2].y, 1.}}};
     if (ABC.det() < 1)
     {
         return; // backface culling + discarding triangles that cover less than a pixel
     }
-
-    auto [bbminx, bbmaxx] = std::minmax({screen[0].x, screen[1].x, screen[2].x}); // bounding box for the triangle
-    auto [bbminy, bbmaxy] = std::minmax({screen[0].y, screen[1].y, screen[2].y}); // defined by its top left and bottom right corners
+    // 包围盒计算
+    auto bbx = std::minmax({screen[0].x, screen[1].x, screen[2].x});
+    auto bbminx = bbx.first;
+    auto bbmaxx = bbx.second;
+    auto bby = std::minmax({screen[0].y, screen[1].y, screen[2].y});
+    auto bbminy = bby.first;
+    auto bbmaxy = bby.second;
+    // 遍历所有像素
 #pragma omp parallel for
     for (int x = std::max<int>(bbminx, 0); x <= std::min<int>(bbmaxx, framebuffer.width() - 1); x++)
-    { // clip the bounding box by the screen
+    {
         for (int y = std::max<int>(bbminy, 0); y <= std::min<int>(bbmaxy, framebuffer.height() - 1); y++)
         {
+            // 计算重心坐标
             vec3 bc = ABC.invert_transpose() * vec3{static_cast<double>(x), static_cast<double>(y), 1.};
-             // barycentric coordinates of {x,y} w.r.t the triangle
             if (bc.x < 0 || bc.y < 0 || bc.z < 0)
             {
-                continue; // negative barycentric coordinate => the pixel is outside the triangle
+                continue;
             }
+            // 插值得到深度z值，进行深度测试，缓存z值
             double z = bc * vec3{ndc[0].z, ndc[1].z, ndc[2].z};
             if (z <= zbuffer[x + y * framebuffer.width()])
             {
                 continue;
             }
             zbuffer[x + y * framebuffer.width()] = z;
+            //绘制
             framebuffer.set(x, y, color);
         }
     }
@@ -110,11 +114,13 @@ int main()
     perspective(norm(eye - center));
     viewport(width / 16, height / 16, width * 7 / 8, height * 7 / 8);//上下左右预留十六分之一的距离
 
-    for (int i=0; i<model.nfaces(); i++) 
-    { // iterate through all triangles
+    // 遍历model中的每个三角形
+    for (int i=0; i<model.nfaces(); i++)
+    {
         vec4 clip[3];
-        for (int d : {0,1,2}) 
-        {            // assemble the primitive
+        // 遍历三角形中的每个顶点，应用MVP变换
+        for (int d : {0,1,2})
+        {
             vec4 v = model.vert(i, d);
             clip[d] = Perspective_M * Model_View_M * vec4{v.x, v.y, v.z, 1.};
         }
